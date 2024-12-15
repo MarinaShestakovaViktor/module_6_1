fe47a32b009a9c0d932b02e30d0c1d150f47fcbc
MarinaShestakovaViktor <151551649+MarinaShestakovaViktor@users.noreply.github.com>
2024-12-15 18:11:18 +0300

    Add files via upload

---
+++ module_6_1.py
@@ -0,0 +1,96 @@
+class Animal:
+    alive = True  # живой
+    fed = False  # накормленный
+
+    def __init__(self, name):
+        self.name = name
+
+    def eat(self, food):
+        if isinstance(food, Plant): # переданное food в классе Plant
+             if food.edible: #  если оно съедобное
+                 print(f'{self.name} съел {food.name}')
+                 self.fed = True
+             else:
+                 print(f'{self.name} не стал есть {food.name}')
+                 self.alive = False
+
+class Plant:
+    edible = False  # съедобность
+    def __init__(self, name):
+        self.name = name
+
+class Mammal(Animal):
+    pass
+class Predator(Animal):
+    pass
+class Flower(Plant):
+    pass
+class Fruit(Plant):
+    #edible = True
+    def __init__(self, name):
+        super().__init__(name)
+        self.edible = True
+
+
+a1 = Predator('Волк с Уолл-Стрит')
+a2 = Mammal('Хатико')
+p1 = Flower('Цветик семицветик')
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+print(a1.alive)
+print(a2.fed)
+a1.eat(p1)
+a2.eat(p2)
+print(a1.alive)
+print(a2.fed)
+
+
+# Задача "Съедобное, несъедобное":
+#
+# Разнообразие животного мира давно будоражит умы человечества. Царства, классы, виды... Почему бы и нам не попробовать выстроить что-то подобное используя наследования классов?
+#
+#
+#
+# Необходимо описать пример иерархии животного мира, используя классы и принцип наследования.
+#
+#
+#
+# Создайте:
+#
+# 2 класса родителя: Animal, Plant
+#
+# Для класса Animal атрибуты alive = True(живой) и fed = False(накормленный), name - индивидуальное название каждого животного.
+#
+# Для класса Plant атрибут edible = False(съедобность), name - индивидуальное название каждого растения
+#
+#
+#
+# 4 класса наследника:
+#
+# Mammal, Predator для Animal.
+#
+# Flower, Fruit для Plant.
+#
+#
+#
+# У каждого из объектов класса Mammal и Predator должны быть атрибуты и методы:
+#
+# eat(self, food) - метод, где food - это параметр, принимающий объекты классов растений.
+#
+# В данном случае можно использовать принцип наследования, чтобы не дублировать код.
+#
+#
+#
+# Метод eat должен работать следующим образом:
+#
+# Если переданное растение (food) съедобное - выводит на экран "<self.name> съел <food.name>", меняется атрибут fed на True.
+#
+# Если переданное растение (food) не съедобное - выводит на экран "<self.name> не стал есть <food.name>", меняется атрибут alive на False.
+#
+# Т.е если животному дать съедобное растение, то животное насытится, если не съедобное - погибнет.
+#
+#
+#
+# У каждого объекта Fruit должен быть атрибут edible = True (переопределить при наследовании)
